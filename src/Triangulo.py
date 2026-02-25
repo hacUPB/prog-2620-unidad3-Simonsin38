2028730f1bc5530ad35783b9cdfa78b9b228a57a
Simón_Osorio <simonsin4001@gmail.com>
2026-02-25 15:36:20 -0500

    Se añaden algunos archivos al repositorio

---
+++ src/Triangulo.py
@@ -0,0 +1,9 @@
+# Calcular el área de un triangulo
+# Inicio
+# Leer base, altura
+# área = base * altura / 2
+# mostrar área
+# Fin
+base = float(input("Ingrese la base:"))
+altura = float(input("Ingrese la altura:"))
+area = (base * altura) / 2
